4805f3e02cd99de6c3edca09e9ddc0fd7b27a280
Matvey <matveynikit@gmail.com>
2026-03-17 00:20:41 +0200

    Refine wallet agreement modal and unify support icon

--- public/js/security-lock.js
+++ public/js/security-lock.js
@@ -91,7 +91,7 @@
 
         <!-- Support FAB (same style as main page) -->
         <button class="security-support-fab" onclick="window.location.href='support.html'">
-          💬
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12z"/></svg>
         </button>
       </div>
     `;
