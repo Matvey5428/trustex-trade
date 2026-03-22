9632d32851bb73e5be591fc9f49078b6c4b92c85
Matvey <matveynikit@gmail.com>
2026-03-22 02:16:48 +0200

    Make DB SSL configurable via DB_SSL env var

--- src/config/database.js
+++ src/config/database.js
@@ -10,7 +10,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
   connectionString: process.env.DB_CONNECTION_STRING || process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
   max: isProduction ? 20 : 5,
   min: isProduction ? 2 : 1,
   idleTimeoutMillis: 30000,
